8cf4a9b5162edac2999ca85c207522ff51d3576a
Soufiane Boukssim <soufianeboukssim8@gmail.com>
2025-05-30 17:20:02 +0100

    commit 54

--- backend/src/main/java/com/soufiane/open_data/services/JwtService.java
+++ backend/src/main/java/com/soufiane/open_data/services/JwtService.java
@@ -37,7 +37,7 @@ public class JwtService {
                 .claims(claims)
                 .subject(email)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis()+1000*1500))
+                .expiration(new Date(System.currentTimeMillis()+1000*50000))
                 .signWith(getKey())
                 .compact();
     }
